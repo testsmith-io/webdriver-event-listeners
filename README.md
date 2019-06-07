804f27f82d33a6995c09da6ed7949d7d559284d2
Roy de Kleijn <dekleijn.roy@gmail.com>
2019-06-07 21:05:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# WebDriver Event Listeners
+
+[![Maven Central](https://img.shields.io/maven-central/v/io.testsmith/webdriver-event-listeners.svg?maxAge=86400)](https://mvnrepository.com/artifact/io.testsmith/webdriver-event-listeners)
+
---
+++ src/main/java/support/events/HighlightElements.java
@@ -0,0 +1,17 @@
+package support.events;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+
+public class HighlightElements extends AbstractWebDriverEventListener {
+
+    @Override
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+    }
+
+}
